d4608660bbcf5e380b0a824b76a032b2db404f3e
MaiconCastello <maicon.kastello@gmail.com>
2022-07-04 18:32:26 -0300

    Layout Homepage - Estático

---
+++ app/src/main/java/com/generation/mycode/adapter/PublicacoesAdapter.kt
@@ -0,0 +1,46 @@
+package com.generation.mycode.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.generation.mycode.databinding.CardHomepageBinding
+import com.generation.mycode.model.Publicacoes
+
+class PublicacoesAdapter: RecyclerView.Adapter<PublicacoesAdapter.PublicacoesViewHolder>(){
+
+    private var listPublicacoes = emptyList<Publicacoes>()
+
+    class PublicacoesViewHolder(val binding: CardHomepageBinding):
+            RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PublicacoesViewHolder {
+    return PublicacoesViewHolder(
+        CardHomepageBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+    )
+    }
+
+    override fun onBindViewHolder(holder: PublicacoesViewHolder, position: Int) {
+        val publicacao = listPublicacoes[position]
+
+        holder.binding.textUsuario.text = publicacao.usuario
+        holder.binding.textCategoria.text = "#${publicacao.categoria}"
+        holder.binding.textDescricaoPublicacao.text = publicacao.conteudo
+        holder.binding.qtdComentarios.text = publicacao.comentario.toString()
+        holder.binding.qtdGood.text = publicacao.good.toString()
+        holder.binding.qtdBad.text = publicacao.bad.toString()
+        holder.binding.qtdComentarios.text = publicacao.comentario.size.toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return listPublicacoes.size
+    }
+
+    fun setList(list: List<Publicacoes>){
+        listPublicacoes =list
+        notifyDataSetChanged()
+    }
+
+}
---
+++ app/src/main/java/com/generation/mycode/model/Comentario.kt
@@ -0,0 +1,4 @@
+package com.generation.mycode.model
+
+class Comentario {
+}
