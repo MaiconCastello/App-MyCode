d11dd44778bc9c17c09b480a0163869272ce9c1e
MaiconCastello <maicon.kastello@gmail.com>
2022-07-05 00:43:15 -0300

    Menu Lateral - Navigation Drawer

--- app/src/main/java/com/generation/mycode/MainActivity.kt
+++ app/src/main/java/com/generation/mycode/MainActivity.kt
@@ -2,20 +2,45 @@ package com.generation.mycode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.generation.mycode.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            toggle = ActionBarDrawerToggle(this@MainActivity, drawerLayout, R.string.open, R.string.close)
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+            navView.setNavigationItemSelectedListener {
+                when(it.itemId){
+                    R.id.perfil -> {Toast.makeText(applicationContext, "Clicou Perfil", Toast.LENGTH_SHORT).show()}
+                    R.id.configuracoes -> {Toast.makeText(applicationContext, "Clicou Configurações", Toast.LENGTH_SHORT).show()}
+                    R.id.logout -> {Toast.makeText(applicationContext, "Clicou Sair", Toast.LENGTH_SHORT).show()}
+                }
+                true
+            }
+        }
+
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_fragment) as NavHostFragment
         navController = navHostFragment.navController
@@ -25,6 +50,15 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 
     private fun replaceFragment(fragment: Fragment){
         if(fragment!=null){
